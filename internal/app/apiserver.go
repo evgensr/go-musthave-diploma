fec312bbc6b954590f55e26bb5a425550cc5556c
evgensr <evgensr@gmail.com>
2022-06-12 16:18:57 +0300

    step4

--- internal/app/apiserver.go
+++ internal/app/apiserver.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 	"net/http"
 
@@ -33,6 +34,11 @@ func newDB(databaseURL string) (*sql.DB, error) {
 	if err := db.Ping(); err != nil {
 		return nil, err
 	}
+
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS  users(\n    id SERIAL PRIMARY KEY,\n    login varchar not null unique,\n    encrypted_password varchar not null\n);\n\n\n\n\nCREATE TABLE IF NOT EXISTS bonuses   (\n    id SERIAL PRIMARY KEY,\n    user_id bigint,\n    order_id bigint,\n    change bigint,\n    type varchar(40) CHECK (type IN ('top_up', 'withdraw')),\n    status varchar(40) CHECK (status in ('NEW', 'REGISTERED', 'INVALID', 'PROCESSING', 'PROCESSED')),\n    change_date timestamp DEFAULT current_timestamp,\n    FOREIGN KEY(user_id) REFERENCES users(id)\n);\n"); err != nil {
+		return nil, errors.New("error sql ")
+	}
+
 	return db, err
 
 }
